5f9afad07467a3a7f3ac2c511bc2d95477cf13ff
Andrew Kennedy <andrew.kennedy@cloudsoftcorp.com>
2014-01-14 21:03:45 +0000

    Bash version 4 update

--- profile.d/bash_completion.sh
+++ profile.d/bash_completion.sh
@@ -1,12 +1,14 @@
 # Check for interactive bash and that we haven't already been sourced.
-[ -z "$BASH_VERSION" -o -z "$PS1" -o -n "$BASH_COMPLETION" ] && return
+[ -z "$BASH_VERSION" -o -z "$PS1" -o -n "$BASH_COMPLETION_COMPAT_DIR" ] && return
 
 # Check for recent enough version of bash.
 bash=${BASH_VERSION%.*}; bmajor=${bash%.*}; bminor=${bash#*.}
-if [ $bmajor -gt 3 ] || [ $bmajor -eq 3 -a $bminor -ge 2 ]; then
-    if shopt -q progcomp && [ -r /opt/local/etc/bash_completion ]; then
+if [ $bmajor -gt 4 ] || [ $bmajor -eq 4 -a $bminor -ge 1 ]; then
+    [ -r "${XDG_CONFIG_HOME:-$HOME/.config}/bash_completion" ] && \
+        . "${XDG_CONFIG_HOME:-$HOME/.config}/bash_completion"
+    if shopt -q progcomp && [ -r /opt/local/share/bash-completion/bash_completion ]; then
         # Source completion code.
-        . /opt/local/etc/bash_completion
+        . /opt/local/share/bash-completion/bash_completion
     fi
 fi
 unset bash bmajor bminor
