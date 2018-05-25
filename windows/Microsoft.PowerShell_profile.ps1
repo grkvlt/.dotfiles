279974948d23c147044296751e6caa8b688368f1
Andrew Donald Kennedy <andrew.kennedy@cloudsoftcorp.com>
2018-05-25 18:30:00 +0100

    Windows PowerShell profile added
    Signed-off-by: Andrew Donald Kennedy <andrew.kennedy@cloudsoftcorp.com>

---
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,12 @@
+﻿# Copyright 2018 by Andrew Donald Kennedy
+#
+# Windows PowerShell profile for current user
+
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+
+# Import Git tab completion and prompt
+Import-Module 'C:\tools\poshgit\dahlbyk-posh-git-9bda399\src\posh-git.psd1'
